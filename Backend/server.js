6d99dbd18278e36df1836d99469f7b2355a465ba
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-09 23:31:31 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,61 @@
+import express from "express";
+import 'dotenv/config';
+import cors from "cors";
+
+const app = express();
+const PORT = 8080;
+
+//for frontend to backend
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`);
+});
+
+app.post("/test", async (req,res)=>{
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-4o-mini",
+            messages: [{
+                role: "user",
+                content: req.body.message
+            }]
+        })
+    };
+    try{
+        const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+        const data = await response.json();
+        //console.log(data.choices[0].message.content);//reply
+        res.send(data.choices[0].message.content);
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+
+
+
+
+
+
+
+// import { GoogleGenAI } from '@google/genai';
+
+// const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: 'gemini-2.0-flash-001',
+//     contents: 'joke about computer science',
+//   });
+//   console.log(response.text);
+// }
+
+// main();
