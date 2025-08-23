edcb8cc21cbf5fe32b022432f6a5e10246d66591
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-24 00:57:25 +0530

    sidebar

--- Frontend/src/SideBar.jsx
+++ Frontend/src/SideBar.jsx
@@ -3,7 +3,25 @@ import "./SideBar.css"
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <section className='sidebar'>
+      {/* new chat button  */}
+      <button>
+        <img src="src/assets/blacklogo.png" alt="chatgpt logo" />
+        <i className="fa-solid fa-pen-to-square"></i>
+      </button>
+
+      {/* history */}
+        <ul className='history'>
+            <li>History1</li>
+            <li>History2</li>
+            <li>History3</li>
+        </ul>
+        
+      {/* sign  */}
+      <div className="sign">
+        <p>By GauravBisen &hearts;</p>
+      </div>
+    </section>
   )
 }
 
