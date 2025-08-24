c1b0e0ea3a4ce67179ee9ad0677b52c7194bdb16
gauravbisen1 <gouravbisen123@gmail.com>
2025-08-24 10:59:29 +0530

    cht window

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./ChatWindow.css"
+import Chat from "./Chat.jsx"
 
 const ChatWindow = () => {
   return (
-    <div>ChatWindow</div>
+    <div className='chatWindow'>
+      <div className='navbar'>
+          <span>Chat Gpt <i className="fa-solid fa-chevron-down"></i></span>
+          <div className="userIconDiv">
+             <span className='userIcon'><i class="fa-solid fa-user"></i></span>
+          </div>
+      </div>
+      <Chat></Chat>
+      <div className="chatInput">
+          <div className="inputBox">
+            <input placeholder='Ask anything'></input>
+            <div id='submit'>
+                <i class="fa-solid fa-paper-plane"></i>
+            </div>
+          </div>
+          <p className='info'>
+          ChatGPT can make mistakes. Check important info. See Cookie Preferences.
+          </p>
+      </div>
+    </div>
   )
 }
 
